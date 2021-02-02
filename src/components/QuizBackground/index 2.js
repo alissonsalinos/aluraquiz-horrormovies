2f4361cae241b016e39f5e99b71fdb23e055f7fe
Alisson Dias <alissonsalinos@outlook.com>
2021-02-02 08:52:59 -0300

    projeto finalizado

---
+++ src/components/QuizBackground/index 2.js
@@ -0,0 +1,9 @@
+// src/components/QuizBackground/index.js
+import styled from 'styled-components';
+
+const QuizBackground = styled.div`
+background: ${({ theme }) => theme.colors.mainBg};
+flex: 1;
+`;
+
+export default QuizBackground;
